ff52c6de147bb0656fe93f7c487efd7ebac09965
srinivasgec <bsrinivasaraocse@giet.ac.in>
2022-10-21 15:30:58 +0530

    just tokens are added

--- src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -5,17 +5,5 @@ public class Calculator {
 		return num1 + num2;
 	}
 
-	public double doSub(double num1, double num2) {
-		//comment line
-		return num1 - num2;
-	}
-
-	public double doMul(double num1, double num2) {
-		return num1 * num2;
-	}
-
-	public double doDiv(double num1, double num2) {
-		return num1 / num2;
-	}
-
+	
 }
